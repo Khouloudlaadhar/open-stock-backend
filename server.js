195f244681b01aa6951788bb420afc7bd02d39d5
khouloud Laadhar <khouloudlaadhar@gmail.com>
2022-03-27 12:50:51 +0200

    Set up framework Express

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+
+app.use((req, res) => {
+    res.json({ message: "Hello from Express" });
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
+});
