5358bdf5f210f1203bfc28d25cddface01ec74e2
khouloud Laadhar <khouloudlaadhar@gmail.com>
2022-03-27 13:16:16 +0200

    Set up database connection

--- server.js
+++ server.js
@@ -1,14 +1,22 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
 require('dotenv').config();
 
 const app = express();
 
 app.use((req, res) => {
-    res.json({ message: "Hello from Express" });
+    res.json({ message: "My APP WEB API v1" });
 });
 
 const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-    console.log(`Server is listening on port ${PORT}`);
-});
+mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true, useUnifiedTopology: true })
+        .then(() => {
+            console.log('Successfully connected to MongoDB');
+            app.listen(PORT, () => {
+                console.log(`Server is listening on port ${PORT}`);
+            });
+        })
+        .catch(error => {
+            console.log(error);
+        })
